b969b5e13e544efd8ebe3010ecc836e0e089feea
Ty Everton <everton_915@yahoo.com>
2021-12-16 14:49:52 -0700

    working on a playRound function - it will take the characters selection and compare it to the computers selection and a winner will be determined

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-computerPlay = () => {
+computerSelection = () => {
   const plays = ['Rock', 'Paper', 'Scissors']
   const computerPlay = plays[Math.floor(Math.random() * plays.length)]
   console.log(computerPlay)
 }
 
-computerPlay()
+playerSelection = () => {
+  const plays = ['Rock', 'Paper', 'Scissors']
+}
+
+playRound = (computerSelection, playerSelection) => {
+  computerSelection = computerSelection()
+  playerSelection = playerSelection()
+  if (computerSelection == playerSelection) {
+    console.log("Tie game!")
+  } else if (
+    (computerSelection == "rock" && playerSelection == "scissors") {
+      console.log("Computer wins this time!")
+    } else if {
+
+    (computerSelection == "paper" && playerSelection == "scissors")   }
+     }
+    else if {
+      
+    }
+    (computerSelection == "scissors" && playerSelection == "paper")
+  ) {
+
+  }
