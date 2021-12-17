2d58604e69ea5fb3e3f15228b778e8ad2de531d1
Ty Everton <everton_915@yahoo.com>
2021-12-17 16:20:24 -0700

    rebuilding entire index.js page as the functions arent working properly

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 const buttons = document.querySelectorAll('input')
+
 computerSelection = () => {
   let plays = ['Rock', 'Paper', 'Scissors']
-  let computerPlay = plays[Math.floor(Math.random() * plays.length)]
+  return plays[Math.floor(Math.random() * plays.length)]
 }
+computerSelection()
+// playRound = (playerSelection) => {
+//   let computerPlay = computerSelection()
+//   let result = ''
+//   if (playerSelection == computerSelection) {
+//     result = 'Tie game!'
+//   } else if (computerPlay == 'rock' && playerSelection == 'scissors') {
+//     result = 'Your rock just took out the computers scissors!'
+//   } else if (computerPlay == 'paper' && playerSelection == 'rock') {
+//     result = 'Nice job! You beat the computer!'
+//   } else if (computerPlay == 'scissors' && playerSelection == 'paper') {
+//     result = 'You win! Scissors overpower the computers paper.'
+//   }
+// }
 
-playRound = (playerSelection) => {
-  let computerPlay = computerSelection()
-  let result = ''
-  if (
-    (playerSelection == 'rock' && computerPlay == 'scissors') ||
-    (playerSelection == 'scissors' && computerPlay == 'paper') ||
-    (playerSelection == 'paper' && computerPlay == 'rock')
-  ) {
-    result =
-      'You win!' + playerSelection + 'beats' + computerPlay + 'every time!'
-  } else if (playerSelection == computerPlay) {
-    result = 'Its a tie! Both users chose' + playerSelection
-  } else {
-    result =
-      'You lose! ' +
-      computerPlay +
-      ' beats ' +
-      playerSelection +
-  }
-  
-  document.getElementById('result').innerHTML = result
-  return
-}
-buttons.forEach((button) => {
-  button.addEventListener('click', function () {
-    playRound(button.value)
-  })
-})
+// buttons.forEach((button) => {
+//   button.addEventListener('click', function () {
+//     playRound(button.value)
+//   })
+// })
+computerSelection()
